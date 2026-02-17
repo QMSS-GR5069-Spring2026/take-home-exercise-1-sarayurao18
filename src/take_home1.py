8251fd05a69982cdfd417cd40ff711bea65ee399
Adeline Setiawan <adelinesetiawan@gmail.com>
2026-02-17 14:49:55 -0500

    Revert "Add data visualization library imports"

--- src/take_home1.py
+++ src/take_home1.py
@@ -1,16 +1,1 @@
 import pandas as pd
-#added other libraries for data visualization
-import seaborn as sns matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
